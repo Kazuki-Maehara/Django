9a027cc3e8de4a6d00726e9f8b00a88f472635e2
Kazuki Maehara <treize.citron13@gmail.com>
2021-09-25 15:56:09 +0900

    modified an example

--- tutorial/polls/tests.py
+++ tutorial/polls/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+    
