88df38309efbe1a51934bedf9fcc51f5fe66feaa
Kazuki Maehara <treize.citron13@gmail.com>
2021-09-28 15:10:17 +0900

    modified an example

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
+
 
 
 class IndexView(generic.ListView):
@@ -13,8 +15,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Rerurn the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Rerurn the last five published questions. (not including those set
+        to be published in the future).
+        """
+        return Question.objects.filter(
+                                    pub_date__lte=timezone.now()
+                                    ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
