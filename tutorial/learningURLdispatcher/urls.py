bcf5d826d4bf40b78a61cabba6d268663c006507
Kazuki Maehara <treize.citron13@gmail.com>
2021-12-31 14:52:12 +0900

    modified and added examples

--- tutorial/learningURLdispatcher/urls.py
+++ tutorial/learningURLdispatcher/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-from . import views
+from django.urls import path, register_converter
+from . import views, converters
 
+
+register_converter(converters.FourDigitYearConverter, 'yyyy')
 
 urlpatterns = [
     path('', views.index, name='index'),
     path(
-        'converter_test/<int:number>/',
+        'converter_test/<yyyy:year>/',
         views.converter_test,
         name='converter_test'
     ),
