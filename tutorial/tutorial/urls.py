f2ce7e277d95c028a41c9f28980afdd17d439528
Kazuki Maehara <treize.citron13@gmail.com>
2022-01-02 14:14:47 +0900

    modified examples

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('URLdispatcher/', include('learningURLdispatcher.urls')),
+    path('templateLearning/', include('templateLearning.urls')),
     path('admin/', admin.site.urls),
 ]
