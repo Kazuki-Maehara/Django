f31a42fba49edc78c91d8e1436734eb8282d029b
Kazuki Maehara <treize.citron13@gmail.com>
2022-01-13 21:12:55 +0900

    modified examples

--- tutorial/learningURLdispatcher/urls.py
+++ tutorial/learningURLdispatcher/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, register_converter
+from django.urls import re_path, path, register_converter
 from . import views, converters
 
 
@@ -8,7 +8,27 @@ urlpatterns = [
     path('', views.index, name='index'),
     path(
         'converter_test/<yyyy:year>/',
+        views.original_test,
+        name='original_test'
+    ),
+    path(
+        'converter_test/<int:number>/',
         views.converter_test,
         name='converter_test'
+    ),
+    re_path(
+        r'^regex_test/(?P<pattern>regex[0-9])/$',
+        views.regex_test,
+        name='regex_test'
+    ),
+    path(
+        'current_datetime/',
+        views.current_datetime,
+        name='current_datetime'
+    ),
+    path(
+        'notFound_test/<str:string>/',
+        views.notFound_test,
+        name='notFound_test'
     ),
 ]
