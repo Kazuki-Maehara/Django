2999f3526937f20026514f30ebce6a62bee3fda2
Kazuki Maehara <treize.citron13@gmail.com>
2022-01-16 14:08:05 +0900

    added an example and modified an example

--- tutorial/aggregationLearning/models.py
+++ tutorial/aggregationLearning/models.py
@@ -7,9 +7,15 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Publisher(models.Model):
     name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -21,7 +27,13 @@ class Book(models.Model):
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     pubdate = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 
 class Store(models.Model):
     name = models.CharField(max_length=300)
     books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
