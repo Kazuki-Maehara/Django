69b4479337f63c01d8a9b84404d594f98afaece5
Kazuki Maehara <treize.citron13@gmail.com>
2022-01-18 21:50:47 +0900

    modified an example

--- tutorial/learningURLdispatcher/views.py
+++ tutorial/learningURLdispatcher/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+import datetime
 
 # Create your views here.
 
@@ -20,3 +22,43 @@ def converter_test(request, number):
         'learningURLdispatcher/converter_test.html',
         context,
     )
+
+
+def original_test(request, year):
+    context = {'x': 'test context'}
+    print(year)
+    return render(
+        request,
+        'learningURLdispatcher/original_test.html',
+        context,
+    )
+
+
+def regex_test(request, pattern):
+    context = {'x': 'test context'}
+    print(pattern)
+    return render(
+        request,
+        'learningURLdispatcher/regex_test.html',
+        context,
+    )
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = """
+<html>
+    <body>
+        It is now {now}.
+    </body>
+</html>
+    """.format(now=now)
+
+    return HttpResponse(html)
+
+
+def notFound_test(request, string):
+    if string == '404':
+        return HttpResponseNotFound('<h1>Pange not found</h1>')
+    else:
+        return HttpResponse('<h1>Page, other than NOT_FOUND</h1>')
