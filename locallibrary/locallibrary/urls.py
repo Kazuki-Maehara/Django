912813253555467ba5ec00b1274ae3261ca1a1e0
Kazuki Maehara <treize.citron13@gmail.com>
2022-02-19 15:02:01 +0900

    added examples

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 
     # Use static() to add url mapping to serve static files during development
-] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
