27b5706b718a516d9cf68fc5c1ba32db43cbd52c
Kazuki Maehara <treize.citron13@gmail.com>
2022-02-20 13:11:22 +0900

    modified an example

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 # Create your views here.
 
@@ -18,13 +19,23 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_book_contains_javascript = Book.objects.filter(
+        title__icontains="javascript"
+    ).count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_book_contains_javascript': num_book_contains_javascript,
     }
 
     # Render the HTML template index.html
     # with the data in the context variable.
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    
